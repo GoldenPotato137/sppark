e3c936c21dc0df20346c36b6a2bd567fdfefe874
goldenpotato <goldenpotato137@gmail.com>
2024-09-12 03:35:07 +0000

    tmp

--- test/main.cu
+++ test/main.cu
@@ -1,10 +1,10 @@
 //
 // Created by xmd on 24-9-11.
 //
-#include <msm/pippenger.hpp>
 #include <cuda.h>
+#include <iostream>
 
-#include <ff/bls12-381.hpp>
+#include <ff/alt_bn128.hpp>
 
 #include <ec/jacobian_t.hpp>
 #include <ec/xyzz_t.hpp>
@@ -14,16 +14,18 @@ typedef xyzz_t<fp_t> bucket_t;
 typedef bucket_t::affine_t affine_t;
 typedef fr_t scalar_t;
 
-#include <msm/pippenger.cuh>
+//#include <msm/pippenger.cuh>
+//
+//RustError mult_pippenger(point_t* out, const affine_t points[], size_t npoints,
+//                         const scalar_t scalars[])
+//{
+//    return mult_pippenger<bucket_t>(out, points, npoints, scalars, false);
+//}
 
-RustError mult_pippenger(point_t* out, const affine_t points[], size_t npoints,
-                         const scalar_t scalars[])
-{
-    return mult_pippenger<bucket_t>(out, points, npoints, scalars, false);
-}
-
+#include <vect.h>
 
 int main()
 {
+    auto tmp = fp_mont(1); //ERROR : this fp_mont is not host side but device side
     return 0;
 }
